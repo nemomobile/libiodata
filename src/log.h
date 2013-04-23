44aa9551815a1441a3f7d49d988722407614b051
Petri M. Gerdt <petri.gerdt@jollamobile.com>
2013-04-23 10:12:13 +0300

    Add copyright header to log.h

--- src/log.h
+++ src/log.h
@@ -1,3 +1,24 @@
+/*-----------------------------------------------------------------------+
+|                                                                        |
+|   Copyright (C) 2013 Jolla Ltd.                                        |
+|   Contact: Petri M. Gerdt <petri.gerdt@jollamobile.com>                |
+|                                                                        |
+|   This file is part of Iodata                                          |
+|                                                                        |
+|   Iodata is free software; you can redistribute it and/or modify       |
+|   it under the terms of the GNU Lesser General Public License          |
+|   version 2.1 as published by the Free Software Foundation.            |
+|                                                                        |
+|   Iodata is distributed in the hope that it will be useful, but        |
+|   WITHOUT ANY WARRANTY;  without even the implied warranty  of         |
+|   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                 |
+|   See the GNU Lesser General Public License  for more details.         |
+|                                                                        |
+|   You should have received a copy of the GNU  Lesser General Public    |
+|   License along with Iodata. If not, see http://www.gnu.org/licenses/  |
+|                                                                        |
++-----------------------------------------------------------------------*/
+
 #ifndef LOG_H
 #define LOG_H
 
